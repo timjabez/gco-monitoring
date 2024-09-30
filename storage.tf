b2af153a257b5ce6e610f205f2fc85671db82e9b
Aravind-google <maravi@google.com>
2024-09-30 13:09:11 +0530

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucket"
+  name          = "no-public-access-bucket-test"
   location      = "US"
   project       = "vivid-ocean-386206"
 }
